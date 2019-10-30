9a9e01664869298a4510b6fa5174d53f110d2e8a
Lisa <lisa.michael@hotmail.com>
2019-10-29 23:51:16 -0400

    added variables for input

--- assets/javascript/moment.js
+++ assets/javascript/moment.js
@@ -12,12 +12,30 @@ document.on("ready", function() {
             appId: "1:96272866734:web:8c19e1459b8ff2cca6bc3f",
             measurementId: "G-VDS17C1TMD"
           
-
-
     }
 
     firebase.initializeApp(config);
   var database = firebase.database();
 
+  $('submit').on('click', function() {
+    var trainName = $('#trainName').val().trim();
+    var destination = $('#destination').val().trim();
+    var time = $('#firstTrainTime').val().trim();
+    // var frequency = moment().diff(startDate, 'months'); 
+    let frequency = $('frequency').val().trim();
+    // console.log(moment()); 
+    // console.log(moment().diff(startDate, 'months'));  
+    // console.log(worked); 
+ 
+database.ref('/newEmployee').push(newEmp)
+
+var newTrain = {
+    trainName: trainName,
+    destination: destination, 
+    time: startDate,
+    frequency: frequency,
+    
+  };
+  });
 
 }); // end of document.on(ready)
