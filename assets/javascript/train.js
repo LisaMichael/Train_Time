9c6dd7e6ab0af957ba29751ff13c39691749f5a4
Lisa <lisa.michael@hotmail.com>
2019-11-03 08:46:20 -0500

    changed code to use train db

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,29 +1,41 @@
 $(document).ready( function() {
 
+  // var firebaseConfig = {
+  //   apiKey: "AIzaSyC4N5iN40dKR8LNLwBw63QawtIxptSQQec",
+  //   authDomain: "lisa-test-oct24.firebaseapp.com",
+  //   databaseURL: "https://lisa-test-oct24.firebaseio.com",
+  //   projectId: "lisa-test-oct24",
+  //   storageBucket: "lisa-test-oct24.appspot.com",
+  //   messagingSenderId: "88210040314",
+  //   appId: "1:88210040314:web:0f83a6c9e1f5f9032a3043",
+  //   measurementId: "G-3WC6LSLK5T"
+  // };
+
+
   var firebaseConfig = {
-    apiKey: "AIzaSyC4N5iN40dKR8LNLwBw63QawtIxptSQQec",
-    authDomain: "lisa-test-oct24.firebaseapp.com",
-    databaseURL: "https://lisa-test-oct24.firebaseio.com",
-    projectId: "lisa-test-oct24",
-    storageBucket: "lisa-test-oct24.appspot.com",
-    messagingSenderId: "88210040314",
-    appId: "1:88210040314:web:0f83a6c9e1f5f9032a3043",
-    measurementId: "G-3WC6LSLK5T"
+    apiKey: "AIzaSyDGwT6UnIscWMnVqa54JFT0Z3oso6JJsbI",
+    authDomain: "traintime-ffb49.firebaseapp.com",
+    databaseURL: "https://traintime-ffb49.firebaseio.com",
+    projectId: "traintime-ffb49",
+    storageBucket: "traintime-ffb49.appspot.com",
+    messagingSenderId: "96272866734",
+    appId: "1:96272866734:web:fc99e51f86ff8da6a6bc3f",
+    measurementId: "G-C2YD3HTPKB"
   };
 
     firebase.initializeApp(firebaseConfig);
   var database = firebase.database();
 
-  $('#submit').on('click', function() {
+  $('#add-train-btn').on('click', function() {
 
     event.preventDefault();
 
-    let trainName = $('#trainName').val().trim();
-    let destination = $('#destination').val().trim();
-    var firstTrainTime = $('#firstTrainTime').val().trim();
+    let trainName = $('#train-name-input').val().trim();
+    let destination = $('#destination-input').val().trim();
+    let firstTrainTime = $('#firstTrainTime-input').val().trim();
     // let firstTrainTime = $('#firstTrainTime').moment(Date()).format('H:mm');
     // var frequency = moment().diff(startDate, 'months'); 
-    let frequency = $('#frequency').val().trim();
+    let frequency = $('#frequency-input').val().trim();
     // console.log(moment()); 
     // console.log(moment().diff(startDate, 'months'));  
     
@@ -56,32 +68,37 @@ database.ref('/newTrain').push(newTrain)
 // dataRef.ref().on("child_added", function (childSnapshot){
 
   database.ref().on("child_added", function(childSnapshot) {
-    // console.log(snapshot.val());
-    let trainName = childSnapshot.val().trainName;
-    let destination = childSnapshot.val().destination;
-    let firstTrainTime = childSnapshot.val().firstTrainTime;
-    let frequency = childSnapshot.val().frequency;
+    console.log(childSnapshot.val());
+
+    // store everything into a variable
+    let trName = childSnapshot.val().trainName;
+    let dest = childSnapshot.val().destination;
+    let fTrainTime = childSnapshot.val().firstTrainTime;
+    let freq = childSnapshot.val().frequency;
     // let rate = childSnapshot.val().rate;
     // let billed = parseInt(childSnapshot.val().billed);
 
-     console.log(childSnapshot.val().trainName);
-     console.log(destination);
-     console.log(firstTrainTime);
+     console.log(trName);
+    //  console.log(destination);
+    //  console.log(firstTrainTime);
     // console.log(rate);
   
     // // // Change the HTML to reflect
-    var tBody = $('#traintable');
-    var tRow = $('<tr>');
-    var trainNameTd = $('<td id="trainName">').text(trainName);
-    var destinationTd = $('<td id="destination">').text(destination);
-    var firstTrainTimeTd = $('<td id="firstTrainTime">').text(firstTrainTime);
-    var frequencyTd = $('<td id="frequency">').text(frequency);
+
+    // let tBody = $('#traintable');
+    // let newRow = $('<tr>');
+    // var trainNameTd = $('<td id="trainName">').text(trainName);
+    // var destinationTd = $('<td id="destination">').text(destination);
+    // var firstTrainTimeTd = $('<td id="firstTrainTime">').text(firstTrainTime);
+    // var frequencyTd = $('<td id="frequency">').text(frequency);
     // var rateTd = $('<td class="rate">').text('$' + rate);
     // var billedTd = $('<td>').text('$' + billed);
 
+     let newRow = $('<tr>');
+
     // Adds new employee to the DOM
-    tRow.append(trainNameTd, destinationTd, firstTrainTimeTd, frequencyTd);
-    tBody.append(tRow);
+    newRow.append(trName, dest, fTrainTime, freq);
+    $('#train-table').append(newRow);
 
 
 
